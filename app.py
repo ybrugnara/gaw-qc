eab4a917cb687ceacfa4a0cd636ac326172d885e
Brugnara <yuri.brugnara@empa.ch>
2024-04-23 15:31:13 +0200

    Run downscaling across multiple CPUs

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ subLOF = LOF(n_neighbors=n_neighbors, metric='euclidean')
 # Define regression model
 ml_model = ExtraTreesRegressor(criterion='squared_error', n_estimators=200, 
                                max_depth=15, max_features=None, 
-                               min_samples_leaf=20, random_state=42)
+                               min_samples_leaf=20, random_state=42, n_jobs=-1)
 
 
 
